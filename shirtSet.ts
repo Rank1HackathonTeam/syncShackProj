66a4d5f923d0a75350507bab3f55501cfe6f02ab
Nathan Lai <naifen.04@gmail.com>
2022-08-28 10:49:34 +1000

    added input fields

--- shirtSet.ts
+++ shirtSet.ts
@@ -1,10 +1,13 @@
-const upper = ['http://clipart-library.com/free/t-shirt-transparent.html',
-'https://purepng.com/photo/2988/black-t-shirt',
-'https://graphicdesign.stackexchange.com/a/21424']
+const upper = ['https://purepng.com/public/uploads/large/purepng.com-black-t-shirtclothingblack-t-shirtfashion-dress-shirt-black-cloth-tshirt-631522326884bzr0p.png',
+'https://purepng.com/public/uploads/large/white-tshirt-n0j.png',
+'https://cdn.shopify.com/s/files/1/0601/9275/1854/products/image_900x900.jpg'];
 
-for (let i = 0; i < upper.length; ++i){
+for (let i = 0; i < upper.length; i++){
     const image = document.createElement('img');
     image.setAttribute('src', upper[i]);
-    image.setAttribute('width', 500);
-    image.setAttribute('height', 300);
+    image.setAttribute('width', '500');
+    image.setAttribute('height', '300');
+    image.setAttribute('class', 'slide');
+    const box = document.getElementById('shirt-container');
+    box.appendChild(image);
 }
